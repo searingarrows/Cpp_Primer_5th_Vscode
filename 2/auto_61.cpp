22ae8a6fb045e96d27a5a8d8468265c1a9078b35
haiyue.zhang <haiyue.zhang@iris-view.com>
2018-10-09 15:43:59 +0800

    typecheck

--- 2/auto_61.cpp
+++ 2/auto_61.cpp
@@ -1,43 +1,43 @@
 #include<iostream>
-#include <typecheck.h>
+#include <check_type.hpp>
+class Foo {};
 /**
  * @brief typeid无法判断是否是引用，且会忽略顶层const
- * 
- * @return int 
+ *
+ * @return int
  */
 int main(){
 	// auto i = 0, *p = &i;
 	// auto sz = 0, pi = 3.14;
 	int i = 0, &r = i;
 	auto a = r;
-	std::cout <<"type name of a is: " << typeid(a).name() << std::endl;
+	std::cout << "type name of a is: " << check_type<decltype(a)>() << std::endl;
 
 	const int ci = i, &cr = ci;
 	auto b = ci;
 	auto c = cr;
 	auto d = &i;
 	auto e = &ci;
-	std::cout << "type name of b is: " << typeid(b).name() << std::endl
-	<< "type name of c is: " << typeid(c).name() << std::endl
-	<< "type name of d is: " << typeid(d).name() << std::endl
-	<< "type name of e is: " << typeid(e).name() << std::endl;
+	std::cout << "type name of b is: " << check_type<decltype(b)>() << std::endl;
+	std::cout << "type name of c is: " << check_type<decltype(c)>() << std::endl;
+	std::cout << "type name of d is: " << check_type<decltype(d)>() << std::endl;
+	std::cout << "type name of e is: " << check_type<decltype(e)>() << std::endl;
 
 	const auto f = ci;
-	std::cout << "type name of f is: " << typeid(f).name() << std::endl;
+	std::cout << "type name of f is: " << check_type<decltype(f)>() << std::endl;
 
 	auto& g = ci;
 	// auto& h = 42;
 	const auto& j = 42;
-	std::cout << "type name of g is: " << typeid(g).name() << std::endl
-	          << "type name of j is: " << typeid(j).name() << std::endl;
+	std::cout << "type name of g is: " << check_type<decltype(g)>() << std::endl;
+	std::cout << "type name of j is: " << check_type<decltype(j)>() << std::endl;
 
 	auto k = ci, &l = i;
 	auto &m = ci, *p = &ci;
 
-	check<decltype(k)>();
-	check<decltype(l)>();
-	check<decltype(m)>();
-	check<decltype(p)>();
-
+	std::cout << "type name of k is: " << check_type<decltype(k)>() << std::endl;
+	std::cout << "type name of l is: " << check_type<decltype(l)>() << std::endl;
+	std::cout << "type name of m is: " << check_type<decltype(m)>() << std::endl;
+	std::cout << "type name of p is: " << check_type<decltype(p)>() << std::endl;
 	return 0;
 }
--- tool/typecheck.h
+++ tool/typecheck.h
@@ -1,27 +1,65 @@
-#include <cxxabi.h> // abi::__cxa_demangle
 #include <iostream>
-#include <typeinfo.h>
-template <typename T> struct check {
-	check(void) {
+#include <typeinfo>
+#if defined(__GNUC__)
+#include <cxxabi.h> // abi::__cxa_demangle
+#endif
+
+template <typename T> struct TypePrint {
+	static std::ostream& Print(std::ostream& os) {
 #if defined(__GNUC__)
 		char* real_name = abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, nullptr);
-		std::cout << real_name;
+		os << real_name;
 		free(real_name);
 #else
-		std::cout << typeid(T).name();
-#endif 
-		}
-		~check(void) { std::cout << std::endl; }
-	};
+		os << typeid(T).name();
+#endif
+		return os;
+	}
+};
 
-#define CHECK_TYPE__(OPT)                                                                          \
-	template <typename T> struct check<T OPT> : check<T> {                                         \
-		check(void) { std::cout << " " #OPT; }                                                     \
+#define TYPE_PREFIX(OPT)                                                                           \
+	template <typename T> struct TypePrint<T OPT> {                                                \
+		static std::ostream& Print(std::ostream& os) {                                             \
+			os << #OPT " ";                                                                        \
+			return TypePrint<T>::Print(os);                                                        \
+		}                                                                                          \
 	};
+#define TYPE_SUFFIX(OPT)                                                                           \
+	template <typename T> struct TypePrint<T OPT> {                                                \
+		static std::ostream& Print(std::ostream& os) {                                             \
+			TypePrint<T>::Print(os);                                                               \
+			os << " " #OPT;                                                                        \
+			return os;                                                                             \
+		}                                                                                          \
+	};
+TYPE_PREFIX(const)
+TYPE_PREFIX(volatile)
+TYPE_PREFIX(const volatile)
+TYPE_SUFFIX(&)
+TYPE_SUFFIX(&&)
+TYPE_SUFFIX(*)
 
-CHECK_TYPE__(const)
-CHECK_TYPE__(volatile)
-CHECK_TYPE__(const volatile)
-CHECK_TYPE__(&)
-CHECK_TYPE__(&&)
-CHECK_TYPE__(*)
+// template <typename T> struct check {
+// 	check(void) {
+// #if defined(__GNUC__)
+// 		char* real_name = abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, nullptr);
+// 		std::cout << real_name;
+// 		free(real_name);
+// #else
+// 		std::cout << typeid(T).name();
+// #endif
+// 	}
+// 	~check(void) { std::cout << std::endl; }
+// };
+
+// #define CHECK_TYPE__(OPT)                                                                          \
+// 	template <typename T> struct check<T OPT> : check<T> {                                         \
+// 		check(void) { std::cout << " " #OPT; }                                                     \
+// 	};
+
+// CHECK_TYPE__(const)
+// CHECK_TYPE__(volatile)
+// CHECK_TYPE__(const volatile)
+// CHECK_TYPE__(&)
+// CHECK_TYPE__(&&)
+// CHECK_TYPE__(*)
